6ab5b145decc3d0f0f65991916d854d654ab238f
vov4ikhid <vovamatsukov7@gmail.com>
2025-09-08 14:49:56 +0300

    Update lab1.2.cpp

--- semester_1/lab1_introduction/lab1.2.cpp
+++ semester_1/lab1_introduction/lab1.2.cpp
@@ -1,30 +1,32 @@
 #include <iostream>
-#include <locale>
+#include <locale> // Добавляем заголовочный файл для setlocale
+
 using namespace std;
 
 int main() {
-	setlocale(0, "");
-	int n;
-	cout << "Ââåäèòå ÷èñëî n: ";
-	cin >> n;
-	int sum = 0;
-	long long work = 1;
-	if (n < 1) {
-		cout << "n äîëæíî áûòü >= 1" << endl;
-		return 1;
-	}
-	for (int i = 1; i <= n; i++) {
-		if (i % 2 == 0) {
-			work *= i;
-		}
-		else {
-			sum += i;
-		}
-	}
-	cout << "Ñóììà âñåõ íå÷¸òíûõ ÷èñåë ðàâíà: " << sum << endl;
-	cout << "Ïðîèçâåäåíèå âñåõ ÷¸òíûõ ÷èñåë ðàâíî: " << work << endl;
+    setlocale(LC_ALL, "Russian"); // Правильное использование setlocale
+    int n;
+    cout << "Введите число n: ";
+    cin >> n;
+    int sum = 0;
+    long long work = 1;
+    
+    if (n < 1) {
+        cout << "n должно быть >= 1" << endl;
+        return 1;
+    }
 
-	return 0;
+    for (int i = 1; i <= n; i++) {
+        if (i % 2 == 0) {
+            work *= i;
+        }
+        else {
+            sum += i;
+        }
+    }
 
+    cout << "Сумма всех нечетных чисел равна: " << sum << endl;
+    cout << "Произведение всех четных чисел равно: " << work << endl;
+
+    return 0;
 }
-
