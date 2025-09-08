2bd060a9760463f9b54af3a3ae145dbbd48c3ac2
vov4ikhid <vovamatsukov7@gmail.com>
2025-09-08 13:36:04 +0300

    Add files via upload

---
+++ semester_1/lab1_introduction/lab1.2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "RU");
+	int n;
+	cout << "Введите число n: ";
+	cin >> n;
+	int sum = 0;
+	long long work = 1;
+	if (n < 1) {
+		cout << "n должно быть >= 1" << endl;
+		return 1;
+	}
+	for (int i = 1; i <= n; i++) {
+		if (i % 2 == 0) {
+			work *= i;
+		}
+		else {
+			sum += i;
+		}
+	}
+	cout << "Сумма всех нечётных чисел равна: " << sum << endl;
+	cout << "Произведение всех чётных чисел равно: " << work << endl;
+
+	return 0;
+}
