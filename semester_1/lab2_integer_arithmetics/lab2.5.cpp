7b2419dffb6134c262389cdf34ec098cd964ff67
vov4ikhid <vovamatsukov7@gmail.com>
2025-09-11 13:45:43 +0300

    Added lab2.5.cpp

---
+++ semester_1/lab2_integer_arithmetics/lab2.5.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+
+int main() {
+	int n;
+	std::cout << "Enter natural integer n: ";
+	if (!(std::cin >> n)) {
+		std::cout << "Error!!!";
+		std::exit(1);
+	}
+	if (n <= 0) {
+		std::cout << "n must be natural!!!";
+		std::exit(1);
+	}
+	for (int i = 1; i <= n; i++) {
+		int rev_i = 0;
+		for (int j = i; j > 0; j /= 10) {
+			rev_i = rev_i * 10 + j % 10;
+		}
+		if (i == rev_i) {
+			long sq = i * i;
+			long rev_sq = 0;
+			for (long j_sq = sq; j_sq > 0; j_sq /= 10) {
+				rev_sq = rev_sq * 10 + j_sq % 10;
+			}
+			if (sq == rev_sq) {
+				std::cout << sq << " - square of " << i << std::endl;
+			}
+		}
+	}
+	return 0;
+}
