00a4fb853caf2fa8717f0dbd6ae1689daa10e97a
vov4ikhid <vovamatsukov7@gmail.com>
2025-09-13 14:03:17 +0300

    lab1.9 update

--- semester_1/lab1_introduction/lab1.9.cpp
+++ semester_1/lab1_introduction/lab1.9.cpp
@@ -1,23 +1,26 @@
 #include<iostream>
-using namespace std;
+
 int main() {
 	setlocale(0, "");
 	int num;
-	cout << "Enter a four digit number: ";
-	cin >> num;
+	std::cout << "Enter a four digit number: ";
+	if (!(std::cin >> num)) {
+		std::cout << "Error!!!";
+		std::exit(1);
+	}
 	if (num < 1000 || num > 9999) {
-		cout << "This number is not four digits.!!!";
-		return 1;
+		std::cout << "This number is not four digits.!!!";
+		std::exit(1);
 	}
 	int dig1 = num / 1000;
 	int dig2 = (num / 100) % 10;
 	int dig3 = (num / 10) % 10;
 	int dig4 = num % 10;
 	if (dig1 == dig4 && dig2 == dig3) {
-		cout << "Number " << num << "- palindrome" << endl;
+		std::cout << "Number " << num << "- palindrome" << std::endl;
 	}
 	else {
-		cout << "Number " << num << "- not palindrome" << endl;
+		std::cout << "Number " << num << "- not palindrome" << std::endl;
 	}
 	return 0;
 }
