4723288632daa1ffa5adad4b93fcacb8b99f7e6b
vov4ikhid <vovamatsukov7@gmail.com>
2025-09-13 19:53:01 +0300

    lab1.4 update

--- semester_1/lab1_introduction/lab1.4.cpp
+++ semester_1/lab1_introduction/lab1.4.cpp
@@ -1,18 +1,21 @@
 #include <iostream>
-using namespace std;
+
 int main() {
 	setlocale(0, "");
 	int k;
 	long long k1 = 1;
 	long long k2 = 1;
-	cout << "Введите число k: ";
-	cin >> k;
+	std::cout << "Enter number k: ";
+	if (!(std::cin >> k)) {
+		std::cout << "Error!!!";
+		std::exit(1);
+	}
 	if (k < 1) {
-		cout << "Число k должно быть >= 1" << endl;
-		return 1;
+		std::cout << "Number k must be >= 1" << std::endl;
+		std::exit(1);
 	}
 	if (k % 2 == 0) {
-		
+
 		for (int i = 2; i <= k; i += 2)
 		{
 			k1 *= i;
@@ -20,14 +23,14 @@ int main() {
 		k = k1;
 	}
 	else {
-		
+
 		for (int i = 1; i <= k; i += 2)
 		{
 			k2 *= i;
 		}
 		k = k2;
 	}
-	cout << "Двойной факториал k = " << k << endl;
+	std::cout << "Double factorial k = " << k << std::endl;
 
 	return 0;
 }
