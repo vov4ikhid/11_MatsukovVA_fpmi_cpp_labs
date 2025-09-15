f2288025fe349248197080a22c49d7b2af7fdac7
vov4ikhid <vovamatsukov7@gmail.com>
2025-09-15 13:20:44 +0300

    lab1.5 update

--- semester_1/lab1_introduction/lab1.5.cpp
+++ semester_1/lab1_introduction/lab1.5.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
 
-using namespace std;
-
 int main() {
     int n, m;
     setlocale(0, "");
-    cout << "Введите натуральное число n: ";
-    cin >> n;
-    cout << "Введите натуральное число m: ";
-    cin >> m;
+    std::cout << "Enter natural number n: ";
+    if (!(std::cin >> n)) {
+        std::cout << "Error!!!";
+        std::exit(1);
+    }
+    std::cout << "Enter natural number m: ";
+    if (!(std::cin >> m)) {
+        std::cout << "Error!!!";
+        std::exit(1);
+    }
     if (n <= 0 || m <= 0) {
-        cout << "ЧИСЛА ДОЛЖНЫ БЫТЬ НАТУРАЛЬНЫМИ" << endl;
-        return 1;
+        std::cout << "NUMBERS MUST BE NATURAL!!!" << std::endl;
+        std::exit(1);
     }
 
     int min = n;
@@ -19,15 +23,15 @@ int main() {
         min = m;
     }
 
-    cout << "Общие делители чисел " << n << " и " << m << ":" << endl;
+    std::cout << "Common divisors of numbers " << n << " and " << m << ":" << std::endl;
 
     for (int i = 1; i <= min; i++) {
         if (n % i == 0 && m % i == 0) {
-            cout << i << " ";
+            std::cout << i << std::endl;
         }
     }
 
-    cout << endl;
+    std::cout << std::endl;
 
     return 0;
 }
