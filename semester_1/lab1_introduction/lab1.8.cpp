f0c3b96da6ee60bdd8e0252d9f56f765ac666e25
vov4ikhid <vovamatsukov7@gmail.com>
2025-09-15 13:55:46 +0300

    lab1.8 update

--- semester_1/lab1_introduction/lab1.8.cpp
+++ semester_1/lab1_introduction/lab1.8.cpp
@@ -1,30 +1,33 @@
 #include<iostream>
-using namespace std;
+
 int main() {
 	setlocale(0, "");
 	const int n = 10;
-		double num[n]; 
-		int q = 0; // переменная количества
-		
-		
-		for (int i = 1; i <= n; i++) {
-			cout << "Введите число " << i << " : ";
-			cin >> num[i];
+	double num[n];
+	int q = 0; // переменная количества
+
+
+	for (int i = 1; i <= n; i++) {
+		std::cout << "Enter number " << i << " : ";
+		if (!(std::cin >> num[i])) {
+			std::cout << "Error!!!";
+			std::exit(1);
 		}
-		// проверка первого числа 
-		if (num[0] < num[1]) {
-			q++; 
+	}
+	// проверка первого числа 
+	if (num[0] < num[1]) {
+		q++;
+	}
+	// проверка остальных чисел
+	for (int i = 2; i <= n - 1; i++) {
+		if (num[i] < num[i + 1] && num[i] < num[i - 1]) {
+			q++;
 		}
-		// проверка остальных чисел
-		for (int i = 2; i <= n - 1; i++) {
-			if (num[i] < num[i + 1] && num[i] < num[i - 1]) {
-				q++;
-			}
-		}
-		// проверка последнего числа
-		if (num[n - 2] > num[n - 1]) {
-			q++; 
-		}
-		cout << "Количесво чисел, которые меньше своих соседей: " << q << endl;
+	}
+	// проверка последнего числа
+	if (num[n - 2] > num[n - 1]) {
+		q++;
+	}
+	std::cout << "The number of numbers that are less than their neighbors: " << q << std::endl;
 	return 0;
 }
