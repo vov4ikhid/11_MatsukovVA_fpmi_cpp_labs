56c908530363b8128ec1cf5866b34733b6afb7c8
vov4ikhid <vovamatsukov7@gmail.com>
2025-09-30 00:16:51 +0300

    Added lab3.14

---
+++ semester_1/lab3_pointers_and_arrays/lab3.14.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+int main() {
+	int n;
+	std::cout << "Enter the number of array elements: ";
+	std::cin >> n;
+	std::vector <double> a(n);
+	std::cout << "Enter " << n << " real numbers: " << "\n";
+	for (int i = 0; i < n; i++) {
+		std::cin >> a[i];
+	}
+	double P;
+	std::cout << "Enter P: ";
+	std::cin >> P;
+	int count_after_P = 0;
+	for (int i = 0; i < n; i++) {
+		if (a[i] > P) {
+			count_after_P++;
+		}
+	}
+	std::cout << "Count of elements larger than " << P << ": " << count_after_P << "\n";
+	int index_max = 0;
+	double max_abs = abs(a[0]), product = 1.0;
+	for (int i = 1; i < n; i++) {
+		if (abs(a[i]) >= max_abs) {
+			max_abs = abs(a[i]);
+			index_max = i;
+		}
+	}
+	if (index_max == n - 1) {
+		std::cout << "There are no elements after the maximum element by modulus!" << "\n";
+	}
+	else {
+		for (int i = index_max + 1; i < n; i++) {
+			product *= a[i];
+		}
+		std::cout << "Product of elements after the maximum in absolute value: " << product << "\n";
+	}
+	std::vector <double> new_a;
+	for (int i = 0; i < n; i++) {
+		if (a[i] < 0) {
+			new_a.push_back(a[i]);
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		if (a[i] >= 0) {
+			new_a.push_back(a[i]);
+		}
+	}
+	std::cout << "New array: " << "\n";
+	for (int i = 0; i < n; i++) {
+		std::cout << new_a[i] << "\n";
+	}
+	return 0;
+}
