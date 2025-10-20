622c7661d9ae183bda09bcf4678cb324bca26103
vov4ikhid <vovamatsukov7@gmail.com>
2025-10-20 17:46:29 +0300

    Lab4.14 ubdate

--- semester_1/lab4_matrices/lab4.14.cpp
+++ semester_1/lab4_matrices/lab4.14.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <iomanip>
+#include <random>
 int task_1(int** matrix, int n) {
 	int sum = 0;
 	for (int j = 0; j < n; j++) {
@@ -28,6 +30,8 @@ void task_2(int** matrix, int n) {
 }
 int main() {
 	int n;
+	std::mt19937 gen(128731);
+	std::uniform_int_distribution<int> dist(-10, 10);
 	std::cout << "Enter the matrix size: ";
 	if (!(std::cin >> n)) {
 		std::cout << "Enter a number!!!";
@@ -44,16 +48,13 @@ int main() {
 	std::cout << "Enter the matrix elements: " << "\n";
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
-			if (!(std::cin >> matrix[i][j])) {
-				std::cout << "Enter numbers!!!";
-				std::exit(1);
-			}
+          matrix[i][j] = dist(gen);
 		}
 	}
 	std::cout << "Matrix:" << "\n";
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
-			std::cout << matrix[i][j] << " ";
+			std::cout << std::setw(4) << matrix[i][j];
 		}
 		std::cout << "\n";
 	}
@@ -63,7 +64,7 @@ int main() {
 	std::cout << "2. Swap the 1st and last lines, the 2nd and the second to last, etc.:" << "\n";
     for (int i = 0; i < n; i++) {
        for (int j = 0; j < n; j++) {
-       std::cout << matrix[i][j] << " ";
+       std::cout << std::setw(4) << matrix[i][j];
 	   }
 	   std::cout << "\n";
 	}
