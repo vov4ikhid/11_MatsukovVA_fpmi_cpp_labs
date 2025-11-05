59e7b2921f8d10ff75d53931e5035b0516fd0904
vov4ikhid <vovamatsukov7@gmail.com>
2025-11-05 17:20:40 +0300

    lab3.32 update

--- semester_1/lab3_pointers_and_arrays/lab3.32.cpp
+++ semester_1/lab3_pointers_and_arrays/lab3.32.cpp
@@ -1,26 +1,6 @@
 #include <iostream>
-
-int main() {
-    int n;
-
-    
-    std::cout << "Enter the number of array elements: ";
-    if (!(std::cin >> n) || n <= 0) {
-        std::cout << "Error!";
-        std::exit(1);
-    }
-  
-    int* a = new int[n];
-
-    std::cout << "Enter " << n << " real numbers: " << "\n";
-    for (int i = 0; i < n; i++) {
-        if (!(std::cin >> a[i])) {
-            std::cout << "Error!";
-            std::exit(1);
-        }
-    }
-
-    int change_of_sign = 0;
+int task_1(int* a, int n) {
+ int change_of_sign = 0;
     for (int i = 0; i < n - 1; i++) {
         bool current = (a[i] >= 0);
         bool next = (a[i + 1] >= 0);
@@ -28,9 +8,10 @@ int main() {
             change_of_sign++;
         }
     }
-    std::cout << "Amount of changes signes: " << change_of_sign << "\n";
-
-    int first_0 = -1, second_0 = -1;
+    return change_of_sign;
+}
+long task_2(int* a, int n) {
+  int first_0 = -1, second_0 = -1;
 
     for (int i = 0; i < n; i++) {
         if (a[i] == 0) {
@@ -53,20 +34,14 @@ int main() {
         for (int i = first_0 + 1; i < second_0; i++) {
             product *= a[i];
         }
-        std::cout << "Product between zeros: " << product << "\n";
+        return product;
     }
     else {
-        std::cout << "No two null elements were found, or there were no elements between them." << "\n";
+        return -1;
     }
-
-    int N;
-    std::cout << "Enter number N: ";
-    if (!(std::cin >> N)) {
-        std::cout << "Error!";
-        std::exit(1);
-    }
-
-    int cur_pos = 0;
+}
+void task_3(int* a, int n, int N) {
+   int cur_pos = 0;
     for (int i = 0; i < n; i++) {
         if (a[i] % N == 0) {
             if (i != cur_pos) {
@@ -79,8 +54,46 @@ int main() {
             cur_pos++;
         }
     }
+}
+int main() {
+    int n;
 
     
+    std::cout << "Enter the number of array elements: ";
+    if (!(std::cin >> n) || n <= 0) {
+        std::cout << "Error!";
+        std::exit(1);
+    }
+  
+    int* a = new int[n];
+
+    std::cout << "Enter " << n << " real numbers: " << "\n";
+    for (int i = 0; i < n; i++) {
+        if (!(std::cin >> a[i])) {
+            std::cout << "Error!";
+            std::exit(1);
+        }
+    }
+
+    
+    std::cout << "Amount of changes signes: " << task_1(a, n) << "\n";
+    if (task_2(a, n) != -1) {
+        std::cout << "Product between zeros: " << task_2(a, n) << "\n";
+    }
+    else {
+        std::cout << "No two null elements were found, or there were no elements between them." << "\n";
+    }
+    
+
+    int N;
+    std::cout << "Enter number N: ";
+    if (!(std::cin >> N)) {
+        std::cout << "Error!";
+        std::exit(1);
+    }
+    
+    task_3(a, n, N);
+
     std::cout << "New array: ";
     for (int i = 0; i < n; i++) {
         std::cout << a[i] << " ";
