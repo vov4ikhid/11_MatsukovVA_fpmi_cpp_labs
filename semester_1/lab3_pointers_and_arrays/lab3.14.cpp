10b2919b7f2d6c735f9cdc9825730bf2c70be07b
vov4ikhid <vovamatsukov7@gmail.com>
2025-11-05 17:03:32 +0300

    lab3.14 updatete

--- semester_1/lab3_pointers_and_arrays/lab3.14.cpp
+++ semester_1/lab3_pointers_and_arrays/lab3.14.cpp
@@ -1,6 +1,48 @@
 #include <iostream>
 #include <cmath>
+int task_1(double* a, int n, double P) {
+    int count_after_P = 0;
+    for (int i = 0; i < n; i++) {
+        if (a[i] > P) {
+            count_after_P++;
+        }
+    }
+    return count_after_P;
+}
+double task_2(double* a, int n) {
+   int index_max = 0;
+    double max_abs = std::abs(a[0]);
 
+    for (int i = 1; i < n; i++) {
+        if (std::abs(a[i]) > max_abs) {
+            max_abs = std::abs(a[i]);
+            index_max = i;
+        }
+    }
+
+    if (index_max == n - 1) {
+        
+        return 0.0;
+    }
+    else {
+        double product = 1.0;
+        for (int i = index_max + 1; i < n; i++) {
+            product *= a[i];
+        }
+        return product;
+    }
+}
+void task_3(double* a, int n) {
+  int index_of_negatives = 0;
+    for (int i = 0; i < n; i++) {
+        if (a[i] < 0) {
+            double temp = a[i];
+            a[i] = a[index_of_negatives];
+            a[index_of_negatives] = temp;
+            index_of_negatives++;
+        }
+    }
+}
 int main() {
     const int MAX_SIZE = 1000;
     double a[MAX_SIZE];
@@ -26,50 +68,15 @@ int main() {
         std::cout << "Error entering a number";
         std::exit(1);
     }
-
-   
-    int count_after_P = 0;
-    for (int i = 0; i < n; i++) {
-        if (a[i] > P) {
-            count_after_P++;
-        }
-    }
-    std::cout << "Count of elements larger than " << P << ": " << count_after_P << "\n";
-
-    
-    int index_max = 0;
-    double max_abs = std::abs(a[0]);
-
-    for (int i = 1; i < n; i++) {
-        if (std::abs(a[i]) > max_abs) {
-            max_abs = std::abs(a[i]);
-            index_max = i;
-        }
-    }
-
-    
-    if (index_max == n - 1) {
+    std::cout << "Count of elements larger than " << P << ": " << task_1(a, n, P) << "\n";
+      if (task_2(a, n) != 0.0) {
+         std::cout << "Product of elements after the maximum in absolute value: " << task_2(a, n) << "\n";
+      }
+      else {
         std::cout << "There are no elements after the maximum element by modulus!" << "\n";
-    }
-    else {
-        double product = 1.0;
-        for (int i = index_max + 1; i < n; i++) {
-            product *= a[i];
-        }
-        std::cout << "Product of elements after the maximum in absolute value: " << product << "\n";
-    }
-
-   
-    int index_of_negatives = 0;
-    for (int i = 0; i < n; i++) {
-        if (a[i] < 0) {
-            double temp = a[i];
-            a[i] = a[index_of_negatives];
-            a[index_of_negatives] = temp;
-            index_of_negatives++;
-        }
-    }
-
+      }
+     task_3(a, n);
+     
     std::cout << "New array: " << "\n";
     for (int i = 0; i < n; i++) {
         std::cout << a[i] << "\n";
