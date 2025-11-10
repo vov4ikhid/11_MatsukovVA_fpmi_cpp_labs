03451bbfe504822ba707143cdc6cf1083bedb4ce
vov4ikhid <vovamatsukov7@gmail.com>
2025-11-11 02:20:13 +0300

    lab3.14 update

--- semester_1/lab3_pointers_and_arrays/lab3.14.cpp
+++ semester_1/lab3_pointers_and_arrays/lab3.14.cpp
@@ -2,7 +2,7 @@
 #include <cmath>
 int task_1(double* a, int n, double P) {
     int count_after_P = 0;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; ++i) {
         if (a[i] > P) {
             count_after_P++;
         }
@@ -13,7 +13,7 @@ double task_2(double* a, int n) {
    int index_max = 0;
     double max_abs = std::abs(a[0]);
 
-    for (int i = 1; i < n; i++) {
+    for (int i = 1; i < n; ++i) {
         if (std::abs(a[i]) > max_abs) {
             max_abs = std::abs(a[i]);
             index_max = i;
@@ -26,7 +26,7 @@ double task_2(double* a, int n) {
     }
     else {
         double product = 1.0;
-        for (int i = index_max + 1; i < n; i++) {
+        for (int i = index_max + 1; i < n; ++i) {
             product *= a[i];
         }
         return product;
@@ -34,7 +34,7 @@ double task_2(double* a, int n) {
 }
 void task_3(double* a, int n) {
   int index_of_negatives = 0;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; ++i) {
         if (a[i] < 0) {
             double temp = a[i];
             a[i] = a[index_of_negatives];
@@ -55,7 +55,7 @@ int main() {
     }
 
     std::cout << "Enter " << n << " real numbers: " << "\n";
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; ++i) {
         if (!(std::cin >> a[i])) {
             std::cout << "Error entering a number";
             std::exit(1);
@@ -78,7 +78,7 @@ int main() {
      task_3(a, n);
 
     std::cout << "New array: " << "\n";
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; ++i) {
         std::cout << a[i] << " ";
     }
 
